38909ae3d8b16f81c6a6a49467cbc82614b8cc6a
lohchuav <victor.loh_chuan_shen@contractors.roche.com>
2022-04-15 17:46:18 +0800

    Angular Unit Testing -Unit test with macrotask,microtask and observables

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -82,15 +82,19 @@ describe('HomeComponent', () => {
   });
 
 
-  it("should display advanced courses when tab clicked", () => {
+  xit("should display advanced courses when tab clicked", (done:DoneFn) => {
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
     fixture.detectChanges();
     const tabs = el.queryAll(By.css(".mat-tab-label"));
     click(tabs[1]);
     fixture.detectChanges();
-    const cardTitles = el.queryAll(By.css('.mat-card-title'));
+    setTimeout(() => {
+      const cardTitles = el.queryAll(By.css('.mat-card-title'));
     expect(cardTitles.length).toBeGreaterThan(0,"could not find card titles");
     expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
+    done();
+    }, 500);
+    
   });
 
 });
